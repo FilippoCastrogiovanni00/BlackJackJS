f49bfb207d98bb127946b22a0e7ca76b3fb6f643
jnk626 <jnk626@pm.me>
2023-03-30 18:21:03 +0200

    aggiunge alcune funzioni di controllo dello score

--- BlackJack/assets/js/blackjack.js
+++ BlackJack/assets/js/blackjack.js
@@ -1,56 +1,55 @@
 "use script";
 
-class Player {
-    constructor(cards_holder) {
-        this.name = null;
-        this.hand = null;
-        this.points = 0;
-        this.isPlayer = true;
-        this.bet = 0;
-        this.budget = 100;
-        this.cards_holder = document.getElementById(cards_holder);
-    }    
+let main = document.getElementById("main");
+main.style.display = "none";
+
+let message = document.createElement("h3");
+main.after(message); 
+
+function Player(cards_holder, player_score) {
+    this.name = "Ciccio";
+    this.hand = [];
+    this.points = 0;
+    this.isPlayer = true;
+    this.bet = 0;
+    this.budget = 100;
+    this.cards_holder = document.getElementById(cards_holder);
+    this.player_score = document.getElementById(player_score);
 }
 
-class Card{
-    constructor (number, suit) {
-        let representation = {
-            hearts: 'c',
-            spades: 'p',
-            diamonds: 'q',
-            clubs: 'f'
-        };
-        this.number = number;
-        if (number == 1) {
-            this.value = 11;
-            this.name = "asso";
-        } else if (number > 1 && number < 11) {
-            this.value = number;
-            this.name = number.toString();
-        } else {
-            this.value = 10;
-            if (number == 11) {
-                this.name = 'j';
-            } else if (number == 12) {
-                this.name = 'q';
-            } else if (number == 13) {
-                this.name = 'k';
-            }
+function Card(number, suit) {
+    let representation = {
+        hearts: 'c',
+        spades: 'p',
+        diamonds: 'q',
+        clubs: 'f'
+    };
+    this.number = number;
+    if (number == 1) {
+        this.value = 11;
+        this.name = "asso";
+    } else if (number > 1 && number < 11) {
+        this.value = number;
+        this.name = number.toString();
+    } else {
+        this.value = 10;
+        if (number == 11) {
+            this.name = 'j';
+        } else if (number == 12) {
+            this.name = 'q';
+        } else if (number == 13) {
+            this.name = 'k';
         }
-        this.suit = suit;
-        this.representation = representation[suit];
-        
     }
+    this.suit = suit;
+    this.representation = representation[suit];
 }
 
-let playerPoints = 0;
-let dealerPoints = 0;
-
-function deal(player){
-   let rn = Math.floor(Math.random()*deck.length);
-   let dealtCard = deck.at(rn);
+function deal(player, deck){
+   let rn = Math.floor(Math.random() * deck.cards.length);
+   let dealtCard = deck.cards.at(rn);
    player.hand.push(dealtCard); 
-   deck.splice(rn, 1);
+   deck.cards.splice(rn, 1);
 
    showHand(player);
 }
@@ -63,11 +62,14 @@ function pointsCount(player){
     player.points = points;
 }
 
-function busted(points){
-    if(points > 21){
-        console.log("you lost")
+function busted(player){
+    if(player.points > 21) {
+        main.style.display = "none";
+        message.style.display = "block";
+        message.textContent = player.name+" busted with " +player.points;
         return true;
     }
+    return false;
 }
 
 function playerWin(dealerPoints, playerPoints){
@@ -79,37 +81,51 @@ function playerWin(dealerPoints, playerPoints){
     return "Loss!";
 }
 
-function show(player, dealer) {
+/* function show(player, dealer) {
     showHand(player);
     showHand(dealer);
-    
+    showScore(player);
+    showScore(dealer);
+} */
+
+function showScore(player) {
+    pointsCount(player);
+    player.player_score.innerHTML = player.points;
 }
 
 function game(){
+    let gameover = false;
+    main.style.display = "block";
+    message.style.display = "none";
     let deck = new Deck();
-    let player = new Player("playerCards");
-    deal(player);
-    deal(player);
-    let dealer = new Player("dealerCards");
+    let player = new Player("playerCards", "playerSum");
+    let dealer = new Player("dealerCards", "dealerSum");
+    dealer.name = "Dealer";
     dealer.isPlayer = false;
     dealer.budget = 10000;
-    deal(dealer);
-    deal(dealer);
 
-    player.points = pointsCount(player);
-    let playerScore = document.getElementById("playerSum");
-    playerScore.innerHTML = player.points;
+    do {
+        deal(player, deck);
+        deal(player, deck);
+        deal(dealer, deck);
+        deal(dealer, deck);
+    
+        showScore(player);
+        showScore(dealer);
+    
+        let hitButton = document.getElementById("hit");
+        hitButton.addEventListener("click", evt => {
+            evt.preventDefault();
+            deal(player, deck);
+            pointsCount(player);
+    
+            showScore(player);
+        })
 
-    dealer.points = pointsCount(dealer);
-    let dealerScore = document.getElementById("dealerSum");
-    dealerScore.innerHTML = dealer.points;
-
-    let tasto = document.getElementById("hit");
-    tasto.addEventListener("click", evt => {
-        evt.preventDefault();
-        deal(player);
-        pointsCount(player);
-    });
+        if (busted(player)) return;
+        if (busted(dealer)) return;
+        
+    } while (player.budget > 0 && dealer.budget > 0 && !gameover);
     
     if(tieBJ()){
         return "tieBJ";
@@ -123,7 +139,7 @@ function game(){
 function showHand(player){
     let c = document.createElement("img");
     c.classList.add("cards");
-    c.src = "../assets/img/"+ hand[hand.length -1].representation + hand[hand.length -1].name + ".png";
+    c.src = "../assets/img/"+ player.hand[player.hand.length -1].representation + player.hand[player.hand.length -1].name + ".png";
     player.cards_holder.appendChild(c);
 }
 
@@ -139,4 +155,5 @@ function tieBJ(){
     return playerPoints== 21 && dealerPoints==playerPoints;        
 }
 
-game();
+let start = document.getElementById("start");
+start.addEventListener("click", game);
--- BlackJack/assets/js/deck.js
+++ BlackJack/assets/js/deck.js
@@ -2,7 +2,7 @@ function Deck() {
     this.suits = ['hearts','spades','diamonds','clubs'];
     this.cards = [];
     for (let i = 1; i < 14; i++) {
-        for (let s of suits) {
+        for (let s of this.suits) {
             let card = new Card(i, s);
             this.cards.push(card);
         }
